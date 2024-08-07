224142e52d306766be0a6192fbdece0e8bac4e76
Drew <dev@anom.ru>
2024-08-07 23:48:12 +0300

    Implement Zone object creation in ZonesTest

--- tests/Method/Zones/ZonesTest.php
+++ tests/Method/Zones/ZonesTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Anktx\Cloud\Dns\Client\Tests\Method\Zones;
 
+use Anktx\Cloud\Dns\Client\Method\Zones\Zone;
 use Anktx\Cloud\Dns\Client\Method\Zones\Zones;
 use PHPUnit\Framework\TestCase;
 
@@ -14,7 +15,7 @@ final class ZonesTest extends TestCase
         $this->expectException(\BadMethodCallException::class);
 
         $zones = new Zones();
-        $zones[0] = $this->zoneObject();
+        $zones[0] = Zone::create($this->zoneObject());
     }
 
     public function testOffsetUnsetException(): void
@@ -28,11 +29,14 @@ final class ZonesTest extends TestCase
     private function zoneObject(): \stdClass
     {
         return (object) [
-            'id' => 'zone-id',
-            'name' => 'zone-name',
-            'type' => 'zone-type',
-            'ttl' => 3600,
-            'records' => [],
+            'id' => 'zone_id',
+            'parent_id' => 'parent_id',
+            'name' => 'zone name',
+            'valid' => true,
+            'validation_text' => 'validation text',
+            'delegated' => true,
+            'created_at' => '2020-01-01T00:00:00.000000Z',
+            'updated_at' => '2020-01-01T00:00:00.000000Z',
         ];
     }
 }
