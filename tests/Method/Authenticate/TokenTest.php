4c4f1fa5f1576c1216f28e1146c5ef6183ab86f8
Drew <dev@anom.ru>
2024-08-08 21:08:45 +0300

    Simplify TokenTest

--- tests/Method/Authenticate/TokenTest.php
+++ tests/Method/Authenticate/TokenTest.php
@@ -5,21 +5,15 @@ declare(strict_types=1);
 namespace Anktx\Cloud\Dns\Client\Tests\Method\Authenticate;
 
 use Anktx\Cloud\Dns\Client\Method\Authenticate\Token;
-use PHPUnit\Framework\TestCase;
+use Anktx\Cloud\Dns\Client\Tests\StubApi;
 
-final class TokenTest extends TestCase
+final class TokenTest extends StubApi
 {
     public function testIsNotExpired(): void
     {
         $ttl = mt_rand(0, 86400);
 
-        $token = Token::create((object) [
-            'access_token' => 'access_token',
-            'id_token' => 'id_token',
-            'scope' => 'scope',
-            'token_type' => 'token_type',
-            'expires_in' => $ttl,
-        ]);
+        $token = Token::create((object) $this->tokenArray(ttl: $ttl));
 
         $this->assertFalse($token->isExpired($this->getTtlDatetime($ttl - 1)));
         $this->assertFalse($token->isExpired($this->getTtlDatetime($ttl)));
